e312d74bced9f400f49d61fa9cfcc6a207c3c7f1
xiangpitang <xiangpitang@2dfire.com>
2016-11-14 16:01:28 +0800

    privilege list line

--- src/components/component.js
+++ src/components/component.js
@@ -4,7 +4,46 @@
 import React from 'react';
 
 export default class Hello extends React.Component {
+
     render() {
-        return <h1>Hello worbgfblgggtrgd</h1>;
+        return (
+            <div>
+                <ul className="privilege-list">
+                    <li className="" >
+                        <div className="circle">V0</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className="">
+                        <div className="circle">V1</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className="red big white">
+                        <div className="circle">V2</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className=" white">
+                        <div className="circle">V3</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className="white" >
+                        <div className="circle">V4</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className="white" >
+                        <div className="circle">V5</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className="white" >
+                        <div className="circle">V6</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                    <li className="white">
+                        <div className="circle">V7</div>
+                        <p className="privilege-name">吃货</p>
+                    </li>
+                </ul>
+            </div>
+
+        );
     }
 }
